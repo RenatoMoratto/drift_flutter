29b604f07db40e3f1cbc896aa582de70bc1212f8
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-08-29 10:34:25 -0300

    refactor: make db global

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:drift_flutter/src/data/local/db/app_db.dart';
 import 'package:drift_flutter/src/page/add_employee_page.dart';
+import 'package:drift_flutter/src/page/edit_employee_page.dart';
 import 'package:drift_flutter/src/page/home_page.dart';
 import 'package:drift_flutter/src/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
+late AppDb db;
+
 void main() {
+  db = AppDb();
+
   runApp(const MyApp());
 }
 
@@ -21,6 +27,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.home: (context) => const HomePage(),
         AppRoutes.addEmployee: (context) => const AddEmployeePage(),
+        AppRoutes.editEmployee: (context) => const EditEmployeePage(),
       },
     );
   }
