c3c7ba4c4b164df64906d0928040bc4e0b2fd3ea
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-08-29 09:45:40 -0300

    feat: list all employees at home page

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,8 +1,28 @@
+import 'package:drift_flutter/src/data/local/db/app_db.dart';
 import 'package:drift_flutter/src/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late AppDb _db;
+
+  @override
+  void initState() {
+    super.initState();
+    _db = AppDb();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _db.close();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +31,61 @@ class HomePage extends StatelessWidget {
         title: const Text("Home"),
         centerTitle: true,
       ),
-      body: Column(
-        children: [],
+      body: FutureBuilder<List<EmployeeData>>(
+        future: _db.getEmployees(),
+        builder: (context, snapshot) {
+          final List<EmployeeData>? employees = snapshot.data;
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          if (employees == null) {
+            return const Text('No data found.');
+          }
+
+          return ListView.builder(
+            itemCount: employees.length,
+            itemBuilder: (context, index) {
+              final employee = employees[index];
+
+              return Card(
+                margin: const EdgeInsets.symmetric(
+                  vertical: 4.0,
+                  horizontal: 12.0,
+                ),
+                shape: const RoundedRectangleBorder(
+                  side: BorderSide(
+                    color: Colors.green,
+                    width: 1.2,
+                    style: BorderStyle.solid,
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(16),
+                    bottomRight: Radius.circular(16),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(employee.id.toString()),
+                      Text(employee.userName.toString()),
+                      Text(employee.firstName.toString()),
+                      Text(employee.lastName.toString()),
+                      Text(employee.dateOfBirth.toString()),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
