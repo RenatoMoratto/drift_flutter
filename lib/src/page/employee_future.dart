285ec14aa38cbe8f14c09db0452dc872e512a5cd
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-08-29 11:01:24 -0300

    feat: bottom navigation at home page

---
+++ lib/src/page/employee_future.dart
@@ -0,0 +1,83 @@
+import 'package:drift_flutter/main.dart';
+import 'package:drift_flutter/src/data/local/db/app_db.dart';
+import 'package:drift_flutter/src/utils/app_routes.dart';
+import 'package:flutter/material.dart';
+
+class EmployeeFuturePage extends StatelessWidget {
+  const EmployeeFuturePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Employee Future"),
+        centerTitle: true,
+      ),
+      body: FutureBuilder<List<EmployeeData>>(
+        future: db.getEmployees(),
+        builder: (context, snapshot) {
+          final List<EmployeeData>? employees = snapshot.data;
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text(snapshot.error.toString()));
+          }
+
+          if (employees == null) {
+            return const Text('No data found.');
+          }
+
+          return ListView.builder(
+            itemCount: employees.length,
+            itemBuilder: (context, index) {
+              final employee = employees[index];
+
+              return GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    AppRoutes.editEmployee,
+                    arguments: employee,
+                  );
+                },
+                child: Card(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 4.0,
+                    horizontal: 12.0,
+                  ),
+                  shape: const RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.green,
+                      width: 1.2,
+                      style: BorderStyle.solid,
+                    ),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(employee.id.toString()),
+                        Text(employee.userName.toString()),
+                        Text(employee.firstName.toString()),
+                        Text(employee.lastName.toString()),
+                        Text(employee.dateOfBirth.toString()),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:drift_flutter/main.dart';
-import 'package:drift_flutter/src/data/local/db/app_db.dart';
+import 'package:drift_flutter/src/page/employee_future.dart';
+import 'package:drift_flutter/src/page/employee_stream.dart';
 import 'package:drift_flutter/src/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
@@ -11,84 +11,47 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int index = 0;
+  final pages = const [
+    EmployeeFuturePage(),
+    EmployeeStreamPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-        centerTitle: true,
-      ),
-      body: FutureBuilder<List<EmployeeData>>(
-        future: db.getEmployees(),
-        builder: (context, snapshot) {
-          final List<EmployeeData>? employees = snapshot.data;
-
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
-
-          if (employees == null) {
-            return const Text('No data found.');
-          }
-
-          return ListView.builder(
-            itemCount: employees.length,
-            itemBuilder: (context, index) {
-              final employee = employees[index];
-
-              return GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(
-                    context,
-                    AppRoutes.editEmployee,
-                    arguments: employee,
-                  );
-                },
-                child: Card(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 4.0,
-                    horizontal: 12.0,
-                  ),
-                  shape: const RoundedRectangleBorder(
-                    side: BorderSide(
-                      color: Colors.green,
-                      width: 1.2,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      bottomRight: Radius.circular(16),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(employee.id.toString()),
-                        Text(employee.userName.toString()),
-                        Text(employee.firstName.toString()),
-                        Text(employee.lastName.toString()),
-                        Text(employee.dateOfBirth.toString()),
-                      ],
-                    ),
-                  ),
-                ),
-              );
-            },
-          );
-        },
-      ),
+      body: pages[index],
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           Navigator.pushNamed(context, AppRoutes.addEmployee);
         },
         icon: const Icon(Icons.add),
         label: const Text('Add Employee'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
+        backgroundColor: Colors.blue.shade300,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.white30,
+        showSelectedLabels: false,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            activeIcon: Icon(Icons.list_outlined),
+            label: 'Employee Future',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            activeIcon: Icon(Icons.list_outlined),
+            label: 'Employee Stream',
+          ),
+        ],
       ),
     );
   }
