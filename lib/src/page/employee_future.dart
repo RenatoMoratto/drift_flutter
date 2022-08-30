9e6e30ed00e08a7af1a0d2253cc1934d1730e871
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2022-08-30 11:55:07 -0300

    refactor: implement consumer at employee future

--- lib/src/page/employee_future.dart
+++ lib/src/page/employee_future.dart
@@ -9,67 +9,65 @@ class EmployeeFuturePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final isLoading = context.select<EmployeeProvider, bool>(
-      (notifier) => notifier.isLoading,
-    );
-    final employees = context.select<EmployeeProvider, List<EmployeeData>>(
-      (notifier) => notifier.employeeListFuture,
-    );
     return Scaffold(
       appBar: AppBar(
         title: const Text("Employee Future"),
         centerTitle: true,
       ),
-      body: isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: employees.length,
-              itemBuilder: (context, index) {
-                final employee = employees[index];
+      body: Consumer<EmployeeProvider>(
+        builder: (context, notifier, child) {
+          if (notifier.isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          }
 
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.pushNamed(
-                      context,
-                      AppRoutes.editEmployee,
-                      arguments: employee,
-                    );
-                  },
-                  child: Card(
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 4.0,
-                      horizontal: 12.0,
+          return ListView.builder(
+            itemCount: notifier.employeeListFuture.length,
+            itemBuilder: (context, index) {
+              final employee = notifier.employeeListFuture[index];
+
+              return GestureDetector(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    AppRoutes.editEmployee,
+                    arguments: employee,
+                  );
+                },
+                child: Card(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 4.0,
+                    horizontal: 12.0,
+                  ),
+                  shape: const RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.green,
+                      width: 1.2,
+                      style: BorderStyle.solid,
                     ),
-                    shape: const RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.green,
-                        width: 1.2,
-                        style: BorderStyle.solid,
-                      ),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(16),
-                        bottomRight: Radius.circular(16),
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(employee.id.toString()),
-                          Text(employee.userName.toString()),
-                          Text(employee.firstName.toString()),
-                          Text(employee.lastName.toString()),
-                          Text(employee.dateOfBirth.toString()),
-                        ],
-                      ),
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
                     ),
                   ),
-                );
-              },
-            ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(employee.id.toString()),
+                        Text(employee.userName.toString()),
+                        Text(employee.firstName.toString()),
+                        Text(employee.lastName.toString()),
+                        Text(employee.dateOfBirth.toString()),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
